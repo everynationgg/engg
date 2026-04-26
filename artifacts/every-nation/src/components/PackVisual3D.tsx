c7bfbd4b87257ca03ffee0aa9f4d383346c4a25e
Yog Depano <everynationgg@gmail.com>
2026-04-26 14:51:02 +0800

    changed the pages 2

--- artifacts/every-nation/src/components/PackVisual3D.tsx
+++ artifacts/every-nation/src/components/PackVisual3D.tsx
@@ -31,38 +31,33 @@ const PackShape = ({ rarity, color, isSelected }: PackVisual3DProps) => {
 
   return (
     <Float speed={isSelected ? 4 : 2} rotationIntensity={isSelected ? 2 : 0.5} floatIntensity={isSelected ? 2 : 1}>
-      <group>
-        {rarity === "common" && (
-          <Tetrahedron ref={meshRef} args={[1.5, 0]}>
-            <meshStandardMaterial {...materialProps} />
-          </Tetrahedron>
-        )}
-        {rarity === "rare" && (
-          <Dodecahedron ref={meshRef} args={[1.2, 0]}>
-            <meshStandardMaterial {...materialProps} />
-          </Dodecahedron>
-        )}
-        {rarity === "epic" && (
-          <group ref={meshRef as any}>
-            <Icosahedron args={[1.4, 1]}>
-              <meshStandardMaterial {...materialProps} wireframe />
-            </Icosahedron>
-            <Icosahedron args={[0.8, 0]}>
-              <meshStandardMaterial {...materialProps} wireframe={false} emissiveIntensity={isSelected ? 1 : 0.5} />
-            </Icosahedron>
-          </group>
-        )}
-        {rarity === "legendary" && (
-          <TorusKnot ref={meshRef as any} args={[0.8, 0.25, 100, 16]}>
-             <MeshDistortMaterial 
-               {...materialProps} 
-               distort={isSelected ? 0.4 : 0.2} 
-               speed={isSelected ? 5 : 2} 
-               emissiveIntensity={isSelected ? 1.5 : 0.5}
-             />
-          </TorusKnot>
-        )}
-      </group>
+      {rarity === "common" ? (
+        <Tetrahedron ref={meshRef} args={[1.5, 0]}>
+          <meshStandardMaterial {...materialProps} />
+        </Tetrahedron>
+      ) : rarity === "rare" ? (
+        <Dodecahedron ref={meshRef} args={[1.2, 0]}>
+          <meshStandardMaterial {...materialProps} />
+        </Dodecahedron>
+      ) : rarity === "epic" ? (
+        <group ref={meshRef as any}>
+          <Icosahedron args={[1.4, 1]}>
+            <meshStandardMaterial {...materialProps} wireframe />
+          </Icosahedron>
+          <Icosahedron args={[0.8, 0]}>
+            <meshStandardMaterial {...materialProps} wireframe={false} emissiveIntensity={isSelected ? 1 : 0.5} />
+          </Icosahedron>
+        </group>
+      ) : (
+        <TorusKnot ref={meshRef as any} args={[0.8, 0.25, 100, 16]}>
+           <MeshDistortMaterial 
+             {...materialProps} 
+             distort={isSelected ? 0.4 : 0.2} 
+             speed={isSelected ? 5 : 2} 
+             emissiveIntensity={isSelected ? 1.5 : 0.5}
+           />
+        </TorusKnot>
+      )}
     </Float>
   );
 };
